285507464371cd86d3ae96a7c3dca6ef3f508e0c
Dimas Azizir <dimaslaylaa@gmail.com>
2025-01-23 16:08:24 +0700

    slicing detail job 3

--- src/app/detail/job/[id]/page.tsx
+++ src/app/detail/job/[id]/page.tsx
@@ -1,8 +1,11 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
+import { Separator } from "@/components/ui/separator";
 import Image from "next/image";
 import Link from "next/link";
 import React, { FC } from "react";
+import { BiCategory } from "react-icons/bi";
 
 interface DetailJobPageProps {}
 
@@ -83,39 +86,64 @@ const DetailJobPage: FC<DetailJobPageProps> = ({}) => {
 
             <div className="mt-6 space-y-4">
               <div className="flex flex-row justify-between">
-                <div className="text-gray-500">
-                  Apply Before
-                </div>
-                <div className="font-semibold">
-                  Juli 31, 2023
-                </div>
+                <div className="text-gray-500">Apply Before</div>
+                <div className="font-semibold">Juli 31, 2023</div>
               </div>
               <div className="flex flex-row justify-between">
-                <div className="text-gray-500">
-                  Job Posted On
-                </div>
-                <div className="font-semibold">
-                  Juli 31, 2023
-                </div>
+                <div className="text-gray-500">Job Posted On</div>
+                <div className="font-semibold">Juli 31, 2023</div>
               </div>
               <div className="flex flex-row justify-between">
-                <div className="text-gray-500">
-                  Job Type
-                </div>
-                <div className="font-semibold">
-                  Full-Time
-                </div>
+                <div className="text-gray-500">Job Type</div>
+                <div className="font-semibold">Full-Time</div>
               </div>
               <div className="flex flex-row justify-between">
-                <div className="text-gray-500">
-                  Salary
-                </div>
-                <div className="font-semibold">
-                  $75k-$85k USD
-                </div>
+                <div className="text-gray-500">Salary</div>
+                <div className="font-semibold">$75k-$85k USD</div>
               </div>
             </div>
           </div>
+
+          <Separator className="my-10" />
+
+          <div>
+            <div className="text-3xl font-semibold">Category</div>
+            <div className="my-10 inline-flex gap-4">
+              <Badge>Marketing</Badge>
+            </div>
+          </div>
+
+          <Separator className="my-10" />
+
+          <div>
+            <div className="text-3xl font-semibold">Required Skills</div>
+            <div className="my-10 inline-flex gap-4">
+              {[0, 1, 2].map((item: number) => (
+                <Badge variant="outline" key={item}>
+                  Marketing
+                </Badge>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="px-32 pb-16">
+        <Separator className="mb-14" />
+
+        <div className="mb-6">
+          <div className="font-semibold text-3xl">Perks & Benefits</div>
+          <div className="text-gray-500 mt-1">This job comes with several perks and benefits</div>
+        </div>
+
+        <div className="grid grid-cols-3 gap-5">
+          {[0, 1, 2].map((item: number) => (
+            <div key={item}>
+              <BiCategory className="w-12 h-12 text-primary" />
+              <div className="font-semibold text-xl mt-6">Full Healthcare</div>
+              <div className="mt-3 text-sm text-gray-500">We believe in thriving communities and that starts with our team being happy and healthy.</div>
+            </div>
+          ))}
         </div>
       </div>
     </>
