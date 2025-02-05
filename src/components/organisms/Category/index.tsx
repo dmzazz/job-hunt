95fba0a209081040049584a690150c09da3bf3f4
Dimas Azizir <dimaslaylaa@gmail.com>
2025-02-05 20:34:42 +0700

    integration fetch data job categories

--- src/components/organisms/Category/index.tsx
+++ src/components/organisms/Category/index.tsx
@@ -1,20 +1,29 @@
-import React, { FC } from "react";
+"use client";
 
-import { BiCategory } from "react-icons/bi";
+import { FC, useMemo } from "react";
+
+import useSWR from "swr";
 
 import TitleSection from "@/components/atoms/TitleSection";
-import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 import CategoryItem from "./CategoryItem";
+import { fetcher, parsingCategories } from "@/lib/utils";
+import { companyJobType } from "@/types";
 
 interface CategoryProps {}
 
 const Category: FC<CategoryProps> = ({}) => {
+  const { data, error, isLoading } = useSWR("/api/job/categories", fetcher);
+
+  const categories = useMemo(() => parsingCategories(data, error, isLoading), [data, error, isLoading]);
+
+  console.log("categories", categories);
+
   return (
     <div className="mt-32 mb-8">
       <TitleSection word1="Explore by" word2="category" />
       <div className="grid grid-cols-5 gap-9 mt-12">
-        {[0, 1, 2, 3].map((item: number) => (
-          <CategoryItem key={item} name="Category" totalJobs={100}/>
+        {categories.map((item: companyJobType) => (
+          <CategoryItem key={item.id} name={item.name} totalJobs={item.totalJobs} />
         ))}
       </div>
     </div>
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,30 +1,36 @@
+export type companyJobType = {
+  id: string;
+  name: string;
+  totalJobs: number;
+};
+
 export type JobType = {
-    image: string;
-    jobType: string;
-    name: string;
-    type: string;
-    location: string;
-    desc: string;
-    categories: string[];
-    needs: number;
-    applicants: number;
-}
+  image: string;
+  jobType: string;
+  name: string;
+  type: string;
+  location: string;
+  desc: string;
+  categories: string[];
+  needs: number;
+  applicants: number;
+};
 
 export type optionType = {
-    id: string
-    label: string
-}
+  id: string;
+  label: string;
+};
 
 export type filterFormType = {
-    label: string
-    name: string
-    items: optionType[]
-}
+  label: string;
+  name: string;
+  items: optionType[];
+};
 
 export type CompanyType = {
-    image: string
-    totalJobs: number
-    name: string
-    description: string
-    categories: string
-}
+  image: string;
+  totalJobs: number;
+  name: string;
+  description: string;
+  categories: string;
+};
